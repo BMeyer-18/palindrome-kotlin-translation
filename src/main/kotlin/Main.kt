091bd1fb94e74ebc5f3fd9ba416540d4b3210183
BMeyer-18 <bzmeyer18@gmail.com>
2026-09-02 17:16:19 -0400

    made initial implementation of kotlin code for all four functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 package org.palindrome
 
 fun main() {
-    println(digitList(50))
+
 }
--- src/main/kotlin/Palindrome.kt
+++ src/main/kotlin/Palindrome.kt
@@ -8,9 +8,13 @@ package org.palindrome
  * @return list of the integer's digits in order of decreasing significance
  */
 fun digitList(integer: Int): List<Int> {
-
-    return listOf()
+    val intList: MutableList<Int> = mutableListOf()
+    for (digit in integer.toString()) {
+        intList.add(digit.code - 48)
+    }
+    return intList.toList()
 }
+
 
 /**
  * Given a list of a positive integer's digits, return a list of digits of the
@@ -20,8 +24,32 @@ fun digitList(integer: Int): List<Int> {
  * @return a list of integer digits for the next largest palindromic integer
  */
 fun nextPalindromeDigits(digits: List<Int>): List<Int> {
-    return listOf()
+    val newDigits: MutableList<Int> = digits.toMutableList()
+    var highMid: Int = newDigits.count() / 2
+    var lowMid: Int = (newDigits.count() - 1) / 2
+    while (highMid < newDigits.count() && lowMid >= 0) {
+        if (newDigits[highMid] == 9) {
+            newDigits[highMid] = 0
+            newDigits[lowMid] = 0
+            highMid += 1
+            lowMid -= 1
+        } else {
+            newDigits[highMid] += 1
+            if (lowMid != highMid) {
+                newDigits[lowMid] += 1
+            }
+            return newDigits.toList()
+        }
+    }
+
+    val finalDigits: MutableList<Int> = mutableListOf(1)
+    for (i in 1 ..< newDigits.count()) {
+        finalDigits.add(0)
+    }
+    finalDigits.add(1)
+    return finalDigits.toList()
 }
+
 
 /**
  * Assemble a list of digits into an integer
@@ -30,8 +58,13 @@ fun nextPalindromeDigits(digits: List<Int>): List<Int> {
  * @return the integer formed from the list of digits in order
  */
 fun assemble(digits: List<Int>): Int {
-    return 0
+    var integer: Int = 0
+    for (digit in digits) {
+        integer = (integer * 10) + digit
+    }
+    return integer
 }
+
 
 /**
  * Given a positive integer whose digits are a palindrome, find the next
@@ -41,5 +74,7 @@ fun assemble(digits: List<Int>): Int {
  * @return the next largest integer whose digits form a palindrome
  */
 fun nextPalindrome(palindromicInt: Int): Int {
-    return 0
+    val digits: List<Int> = digitList(palindromicInt)
+    val nextDigits: List<Int> = nextPalindromeDigits(digits)
+    return assemble(nextDigits)
 }
